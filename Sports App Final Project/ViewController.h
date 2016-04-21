92e19c43f7060e14c0f6d6957d55b021a3be8e08
Nikhil Raman <Nikhil@levyvlan502.2015.wlan.wireless-resnet.upenn.edu>
2016-04-21 11:20:56 -0400

    Initial Commit

---
+++ Sports App Final Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sports App Final Project
+//
+//  Created by Nikhil Raman on 4/21/16.
+//  Copyright © 2016 cis195. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sports App Final Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sports App Final Project
+//
+//  Created by Nikhil Raman on 4/21/16.
+//  Copyright © 2016 cis195. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
